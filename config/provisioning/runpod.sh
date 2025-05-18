2d4fb2143aac85d6b6901df4063621ff6539a9a0
Senthil Kumar Palanivelu <senthilkumar.palanivel@gmail.com>
2025-05-18 10:38:53 +0530

    Using Axel to download

--- config/provisioning/runpod.sh
+++ config/provisioning/runpod.sh
@@ -201,9 +201,9 @@ function provisioning_start() {
     fi
 
     provisioning_print_header
-    provisioning_get_apt_packages
-    provisioning_get_pip_packages
-    provisioning_get_nodes
+#    provisioning_get_apt_packages
+#    provisioning_get_pip_packages
+#    provisioning_get_nodes
     provisioning_get_models \
         "${WORKSPACE}/ComfyUI/models/ckpt" \
         "${CHECKPOINT_MODELS[@]}"
