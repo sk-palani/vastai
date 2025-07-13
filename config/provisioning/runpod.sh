59a8c814d3f80dca4062462f9e2c203fc908a412
Senthil Kumar <senthilkumar.palanivel@gmail.com>
2025-07-13 13:55:36 +0530

    Fix to the provisioning script   - New Custom Nodes

--- config/provisioning/runpod.sh
+++ config/provisioning/runpod.sh
@@ -422,9 +422,9 @@ function provisioning_download() {
 
 }
 
-provisioning_get_apt_packages
-provisioning_get_nodes
-provisioning_get_pip_packages
+#provisioning_get_apt_packages
+#provisioning_get_nodes
+#provisioning_get_pip_packages
 
 # Allow user to disable provisioning if they started with a script they didn't want
 if [[ ! -f /workspace/.noprovisioning ]]; then
