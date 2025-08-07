0cec33266ece3cc7238afc368f534b3c4d78d729
Senthil Kumar <senthilkumar.palanivel@gmail.com>
2025-08-08 00:55:53 +0530

    Fix to the provisioning script   - New Lora

--- config/provisioning/runpod.sh
+++ config/provisioning/runpod.sh
@@ -209,6 +209,7 @@ LORA_MODELS=(
     "https://civitai.com/api/download/models/1885706?type=Model&format=SafeTensor&token=${CIVITAI_TOKEN}"
     "https://civitai.com/api/download/models/1943855?type=Model&format=SafeTensor&token=${CIVITAI_TOKEN}"
     "https://civitai.com/api/download/models/1553172?type=Model&format=SafeTensor&token=${CIVITAI_TOKEN}"
+    "https://civitai.com/api/download/models/1245565?type=Model&format=SafeTensor&token=${CIVITAI_TOKEN}"
 
 )
 
