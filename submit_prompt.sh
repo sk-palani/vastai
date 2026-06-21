c4b685002c9f6aa0e16cb136dff1516343905f77
senthil <senthilkumar.palanivel@gmail.com>
2026-06-21 12:09:49 +0530

    New Workflow_API.json (35)

--- submit_prompt.sh
+++ submit_prompt.sh
@@ -28,7 +28,7 @@ UPDATED_FILE="$SCRIPT_DIR/Workflow_API_updated.json"
 LOG_TS=$(date '+%Y-%m-%d %H:%M:%S')
 
 # -- Download latest workflow file
-DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/31_FULL_Workflow_API.json"
+DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/35_FULL_Workflow_API.json"
 
 wget -O "${WORKFLOW_FILE}" "${DEFAULT_WORKFLOW}"
 echo "Hash : " "$(md5sum ${WORKFLOW_FILE})"
