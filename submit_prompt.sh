fa1ce16ce53e4460094f0db6d54b02239ba02d1d
senthil <senthilkumar.palanivel@gmail.com>
2026-07-07 08:54:38 +0530

    3M workflows

--- submit_prompt.sh
+++ submit_prompt.sh
@@ -28,7 +28,8 @@ UPDATED_FILE="$SCRIPT_DIR/Workflow_API_updated.json"
 LOG_TS=$(date '+%Y-%m-%d %H:%M:%S')
 
 # -- Download latest workflow file
-DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/006_KRWorkflow_API.json"
+DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/0001_3MWorkflow_API.json"
+# DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/006_KRWorkflow_API.json"
 #DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/004_KFWorkflow_API.json"
 #DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/35_FULL_Workflow_API.json"
 
