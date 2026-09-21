cffe2ed36852cda3077acd556525b71a3a7bb508
Senthil Kumar Palanivelu <senthilkumar.palanivel@gmail.com>
2026-09-21 09:11:27 +0530

    Gold Workflow  0001gm FFF6DC

--- submit_prompt.sh
+++ submit_prompt.sh
@@ -28,7 +28,8 @@ UPDATED_FILE="$SCRIPT_DIR/Workflow_API_updated.json"
 LOG_TS=$(date '+%Y-%m-%d %H:%M:%S')
 
 # -- Download latest workflow file
-DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/0011_3MWorkflow_API.json"
+#DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/0011_3MWorkflow_API.json"
+DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/g0001_3MGWorkflow_API.json"
 #DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/0003_3MWorkflow_API.json"
 # DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/006_KRWorkflow_API.json"
 #DEFAULT_WORKFLOW="https://raw.githubusercontent.com/sk-palani/vastai/refs/heads/main/workflows/004_KFWorkflow_API.json"
